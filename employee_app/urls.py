b031ae31a2e01281e49353c821ad1d00cc03457c
Somaiya-0 <sdark.babe@gmail.com>
2025-06-26 23:51:10 +0600

    view and delete working

--- employee_app/urls.py
+++ employee_app/urls.py
@@ -21,7 +21,7 @@ from . import views
 urlpatterns = [
     path('',views.home,name='home'),
     path('add/', views.add_employee, name='add_employee'),
-    path('update/<int:id>/', views.update_employee, name='update_employee'),
-    path('delete/<int:id>/',views.delete_employee,name='delete_employee')
+    path('update/<int:emp_id>/', views.update_employee, name='update_employee'),
+    path('delete/<int:emp_id>/',views.delete_employee,name='delete_employee')
 ]
  
--- employee_app/views.py
+++ employee_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render,HttpResponse
 from .models import Department,role,Employee
+from datetime import date
 
 # Create your views here.
 def home(request):
@@ -12,13 +13,31 @@ def home(request):
 
 def add_employee(request):
     if request.method == 'POST':
-        print('y')
-    else:
-        print('n')
-    return render(request,'add_employee.html')
+        name= request.POST['name']
+        email=  request.POST['email']
+        Position= int(request.POST['Position'])
+        dept= int(request.POST['dept'])
+        blood_group= request.POST['blood_group']
+        salary= int(request.POST['salary'])
+        phone= int(request.POST['phone'])
 
-def delete_employee(request,id):
-    return render(request,'index.html')
+        employee= Employee(name=name, email=email, Position_id = Position, dept_id=dept, blood_group=blood_group, salary= salary, hire_date=date.today(), phone=phone)
+        employee.save()
+        return redirect('home')
+    elif request.method=='GET':
+        print("get")
+        
+        return render(request,'add_employee.html')
 
-def update_employee(request,id):
+def delete_employee(request,emp_id=0):
+    if emp_id:
+        emp_del = Employee.objects.get(id=emp_id)
+        emp_del.delete()
+        return redirect('home')
+
+    #return render(request,'index.html')
+
+def update_employee(request,emp_id):
+    if emp_id:
+        emp_update= Employee.objects.get(id=emp_id)
     return render(request,'update_employee.html')
